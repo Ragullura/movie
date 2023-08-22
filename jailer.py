cbf530f013c50fa944714a4960310aaf07f3c13f
Ragullura <ragullura.005@gmail.com>
2023-08-22 16:47:57 +0530

    added casting name

--- jailer.py
+++ jailer.py
@@ -4,4 +4,8 @@ print("music Anirudh")
 
 print ("Kalanidhi Maran produced this film under his own production banner, Sun Pictures.")
 
-
+print("casting Name")
+print("Yogi babu comedian")
+print("Tamanah")
+print("Mohanlal")
+print("vinayakam")
